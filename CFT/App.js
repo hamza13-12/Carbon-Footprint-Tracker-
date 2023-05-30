f18a6aec9ebe075e7e9eab37c0cc5613221f6d73
hamza13-12 <hamzaahmad277@gmail.com>
2023-05-30 20:05:25 +0500

    Added a login button

--- CFT/App.js
+++ CFT/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image, Dimensions, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, Dimensions, TextInput, TouchableOpacity, Keyboard, Button } from 'react-native';
 
 export default function App() {
   const { width, height } = Dimensions.get('window');
