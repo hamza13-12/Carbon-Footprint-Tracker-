14de7e1a7f171fcab3e922cbc6c3f90ce64fa5ef
hamza13-12 <hamzaahmad277@gmail.com>
2023-05-30 13:42:39 +0500

    Added a background image

---
+++ CFT/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require("./assets/bg.png")} style={styles.backgroundImage}>
+        <Text>Placeholder</Text>
+      </ImageBackground>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+});
